0b333539b4bc7b45e11795509331ff9a88b623ba
ayeV <36230640+ayeV@users.noreply.github.com>
2018-04-10 17:42:16 -0300

    Add files via upload

--- TP_1_Cascara/funciones.c
+++ TP_1_Cascara/funciones.c
@@ -1,4 +1,11 @@
-//sumar recibe dos numeros float, los suma y retorna resultado de tipo float
+/** \funcion que suma dos numeros tipo float
+ *
+ * \param primero numero que recibe tipo float
+ * \param segundo numero que recibe tipo float
+ * \return resultado de la suma
+ *
+ */
+
 
 float sumar(float a, float b){
 
@@ -7,28 +14,55 @@ float sumar(float a, float b){
 
     return resultado;
 }
-//restar recibe dos float, los resta y retorna el resultado de tipo float
+/** \funcion que resta dos numeros tipo float
+ * \param 1er numero tipo float
+ * \param 2do numero tipo float
+ * \return rsultado de la resta
+ *
+ */
+
 float restar(float c, float d){
     float resultado;
         resultado = c-d;
     return resultado;
 
 }
-//division recibe dos float, los divide, y retorna el resultado tipo float
+/** \brief funcion que divide dos numeros tipo float
+ *
+ * \param 1er nro tipo float
+ * \param 2do nro tipo float
+ * \return resultado de la division
+ *
+ */
+
 float division (float e, float f){
     float resultado;
 
         resultado = e/f;
     return resultado;
   }
-//multiplicacion recibe dos float, los multiplica y retorna el resultado tipo float
+/** \brief funcion que multiplica dos nros
+ *
+ * \param 1er nro float
+ * \param 2do nro float
+ * \return resultado de la multiplicacion
+ *
+ */
+
 float multiplicacion(float g, float h){
     float resultado;
 
     resultado = g*h;
     return resultado;
 }
-//factorial recibe un parametro tipo entero, calcula el factorial y retorna el resultado de tipo entero
+/** \brief funcion que calcula el factorial de un nro
+ *
+ * \param unico nro tipo float que recibe
+ *
+ * \return resultado del factorial
+ *
+ */
+
 int factorial(int i){
     int resultado;
         if(i == 1){
